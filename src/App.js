56ae89ee4fd74e3f00ecc3b6b264ec24a6c8de8f
the-nicholas-holland <the.nicholas.holland@gmail.com>
2026-05-19 13:02:53 -0500

    updated readme

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
-  return (
+    return (
     <div className="App">
       <Navbar />
+      <h1>HELLO</h1>
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,13 +4,13 @@ import "./Navbar.css";
 
 const Navbar = () => {
     return <nav className="navbar">
-        <div className="navbar_links">
+        <ul className="navbar_links">
             <a href="">home</a>
             <a href="">projects</a>
             <a href="">games</a>
             <a href="">about</a>
             <a href="">resume</a>
-        </div>
+        </ul>
     </nav>
 };
 
